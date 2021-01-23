9d42b637d155d3d0bca188402035dcaf4877f5f1
Enzo Dal Cerro <gmx1pt@gmail.com>
2021-01-23 16:14:18 +0100

    3 init

---
+++ philo_three/head/philo_two.h
@@ -0,0 +1,62 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo_two.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: edal--ce <edal--ce@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/03/18 18:34:51 by edal--ce          #+#    #+#             */
+/*   Updated: 2021/01/23 16:11:36 by edal--ce         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILO_TWO_H
+# define PHILO_TWO_H
+
+# include <stdio.h>
+# include <string.h>
+# include <sys/time.h>
+# include <semaphore.h>
+# include <pthread.h>
+# include <stdlib.h>
+# include <unistd.h>
+
+# define FORK 0
+# define EAT 1
+# define SLEEP 2
+# define THINK 3
+# define DIE 4
+
+typedef	struct		s_contr
+{
+	int				nbr_of_philo;
+	int				time_to_die;
+	int				time_to_eat;
+	int				time_to_sleep;
+	int				must_eat;
+	int				did_eat;
+	char			end;
+	sem_t			**forks;
+	struct timeval	start;
+}					t_contr;
+typedef	struct		s_philo
+{
+	int				id;
+	int				alive;
+	struct timeval	lmeal;
+	sem_t			*alive_l;
+	t_contr			*contr;
+}					t_philo;
+
+struct s_contr		*contr;
+int					init_contr(t_contr *contr, char **argv, int argc);
+void				print_ts(t_philo *phil, int action);
+void				life(t_philo *phil);
+int					eat(t_philo *phil);
+int					check_alive(t_philo *phil);
+int					ft_atoi(const char *in);
+int					ft_strlen(char *str);
+char				*ft_itoa(unsigned long n);
+size_t				ft_strlcat(char *dst, char *src, size_t size);
+
+#endif
---
+++ philo_three/src/philo_two.c
@@ -0,0 +1,105 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo_two.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: edal--ce <edal--ce@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/11/01 18:14:58 by edal              #+#    #+#             */
+/*   Updated: 2021/01/23 16:12:17 by edal--ce         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "philo_two.h"
+
+void	loop(t_philo *phil)
+{
+	int cpt;
+
+	phil->alive = 1;
+	cpt = 0;
+	sem_wait(phil->alive_l);
+	gettimeofday(&(phil->lmeal), 0);
+	sem_post(phil->alive_l);
+	while (phil->alive && !phil->contr->end)
+	{
+		print_ts(phil, THINK);
+		eat(phil);
+		if (++cpt == phil->contr->must_eat)
+		{
+			phil->contr->did_eat++;
+			phil->alive = 0;
+			break ;
+		}
+		print_ts(phil, SLEEP);
+		usleep(phil->contr->time_to_sleep * 1000);
+	}
+	return ;
+}
+
+char	*modbuf(char *buff, int i)
+{
+	buff[0] = 'A';
+	buff[2] = 0;
+	buff[1] = '0' + i;
+	return (buff);
+}
+
+void	spawn_philos(t_contr *contr)
+{
+	int			i;
+	pthread_t	pid_tot[2][contr->nbr_of_philo];
+	t_philo		philos[contr->nbr_of_philo];
+	char		buff[20];
+
+	i = -1;
+	gettimeofday(&(contr->start), 0);
+	while (++i < contr->nbr_of_philo)
+	{
+		philos[i].contr = contr;
+		philos[i].id = i;
+		gettimeofday(&(philos[i].lmeal), 0);
+		sem_unlink(modbuf(buff, i));
+		philos[i].alive_l = sem_open(modbuf(buff, i), O_CREAT, 0644, 1);
+		pthread_create(&(pid_tot[0][i]), 0, (void*)loop, (void*)&(philos[i]));
+		pthread_create(&(pid_tot[1][i]), 0, (void*)life, (void*)&(philos[i]));
+		usleep(50);
+	}
+	i = -1;
+	while (++i < contr->nbr_of_philo)
+	{
+		pthread_join(pid_tot[0][i], 0);
+		pthread_join(pid_tot[1][i], 0);
+		sem_close(philos[i].alive_l);
+	}
+}
+
+void	cleanup(t_contr *contr)
+{
+	int i;
+
+	i = 0;
+	while (i < contr->nbr_of_philo)
+		sem_close(contr->forks[i++]);
+	free(contr->forks);
+}
+
+int		main(int argc, char **argv)
+{
+	t_contr contrn;
+
+	if (argc < 5 || argc > 6)
+	{
+		write(1,
+"nbr of philo | time_to_die | time_to_eat | time_to_sleep | [must eat]\n", 70);
+		return (0);
+	}
+	contr = &contrn;
+	if (init_contr(contr, argv, argc))
+		return (1);
+	spawn_philos(&contrn);
+	if (contr->did_eat == contr->nbr_of_philo)
+		write(1, "All philos ate as supposed\n", 27);
+	cleanup(&contrn);
+	return (0);
+}
